c8c34a6574edd8343cf1ac766dad0c0f533482c5
TheRealKrishna <agarwl.krishna2@gmail.com>
2024-11-28 11:23:33 +0530

    Refactor BFS implementation to use adjacency matrix and improve clarity

--- Lab Record/24.cpp
+++ Lab Record/24.cpp
@@ -2,32 +2,23 @@
 
 #include <iostream>
 #include <queue>
-#include <vector>
-
 using namespace std;
-
-void BFS(int start, const vector<vector<int>>& adj) {
-  vector<bool> visited(adj.size(), false);
+int main() {
+  int g[4][4] = {{0, 1, 1, 0}, {1, 0, 1, 1}, {1, 1, 0, 1}, {0, 1, 1, 0}};
+  bool v[4] = {0};
   queue<int> q;
-  q.push(start);
-  visited[start] = true;
-
+  q.push(0);
+  v[0] = 1;
   while (!q.empty()) {
-    int node = q.front();
+    int n = q.front();
     q.pop();
-    cout << node << " ";
-
-    for (int neighbor : adj[node]) {
-      if (!visited[neighbor]) {
-        visited[neighbor] = true;
-        q.push(neighbor);
+    cout << n << " ";
+    for (int i = 0; i < 4; i++) {
+      if (g[n][i] && !v[i]) {
+        q.push(i);
+        v[i] = 1;
       }
     }
   }
-}
-
-int main() {
-  vector<vector<int>> adj = {{1, 2}, {0, 3, 4}, {0, 4}, {1, 5}, {1, 2}, {3}};
-  BFS(0, adj);
   return 0;
 }
